611c8f823b5cf8858431a49376eca4ca800213c3
jakeprem <jake@vetspire.com>
2023-12-19 12:39:03 -0500

    Add Rustler precompiled

--- lib/merge_pdf/native.ex
+++ lib/merge_pdf/native.ex
@@ -2,7 +2,14 @@ defmodule MergePdf.Native do
   @moduledoc """
   Documentation for `MergePdf.Native`.
   """
-  use Rustler, otp_app: :merge_pdf, crate: "mergepdf_native"
+  version = Mix.Project.config()[:version]
+
+  use RustlerPrecompiled,
+    otp_app: :merge_pdf,
+    crate: "mergepdf_native",
+    base_url: "https://github.com/jakeprem/merge_pdf/releases/download/v#{version}",
+    force_build: System.get_env("RUSTLER_PRECOMPILATION_MERGE_PDF_BUILD") in ["1", "true"],
+    version: version
 
   def merge_binaries(_binaries), do: :erlang.nif_error(:nif_not_loaded)
 
--- mix.exs
+++ mix.exs
@@ -1,13 +1,31 @@
 defmodule MergePdf.MixProject do
   use Mix.Project
 
+  # Used by CI/CD, so make sure this is updated
+  @version "0.1.0"
+  @source_url "https://github.com/jakeprem/merge_pdf"
+
   def project do
     [
       app: :merge_pdf,
-      version: "0.1.0",
+      version: @version,
       elixir: "~> 1.15",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      package: package()
+    ]
+  end
+
+  defp package do
+    [
+      files: [
+        "lib",
+        "native",
+        "checksum-*.exs",
+        "mix.exs"
+      ],
+      licenses: ["MIT"],
+      links: %{"GitHub" => @source_url}
     ]
   end
 
@@ -21,7 +39,8 @@ defmodule MergePdf.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:rustler, "~> 0.30.0"}
+      {:rustler_precompiled, "~> 0.7"},
+      {:rustler, "~> 0.30.0", optional: true}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
     ]
