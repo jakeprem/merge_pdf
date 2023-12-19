8cd3272cb1adcdeb3e3b4f1b14c647f0950304a8
jakeprem <jake@vetspire.com>
2023-12-19 14:07:14 -0500

    Small mix.exs updates

--- mix.exs
+++ mix.exs
@@ -4,15 +4,26 @@ defmodule MergePdf.MixProject do
   # Used by CI/CD, so make sure this is updated
   @version "0.0.1"
   @source_url "https://github.com/jakeprem/merge_pdf"
+  @dev? String.ends_with?(@version, "-dev")
+  @force_build? System.get_env("MERGE_PDF_BUILD") in ["1", "true"]
 
   def project do
     [
       app: :merge_pdf,
+      name: "Merge PDF",
+      description: "Merge PDFs using lopdf via Rustler",
       version: @version,
       elixir: "~> 1.15",
-      start_permanent: Mix.env() == :prod,
+      package: package(),
       deps: deps(),
-      package: package()
+      docs: docs()
+    ]
+  end
+
+  # Run "mix help compile.app" to learn about applications.
+  def application do
+    [
+      extra_applications: [:logger]
     ]
   end
 
@@ -29,20 +40,22 @@ defmodule MergePdf.MixProject do
     ]
   end
 
-  # Run "mix help compile.app" to learn about applications.
-  def application do
-    [
-      extra_applications: [:logger]
-    ]
-  end
-
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
       {:rustler_precompiled, "~> 0.7"},
-      {:rustler, "~> 0.30.0", optional: true}
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      {:rustler, "~> 0.30.0", optional: not (@dev? or @force_build?)},
+
+      # Dev
+      {:ex_doc, ">= 0.0.0", only: :dev, runtime: false}
+    ]
+  end
+
+  defp docs do
+    [
+      main: "Merge PDF",
+      source_ref: "v#{@version}",
+      source_url: @source_url
     ]
   end
 end
