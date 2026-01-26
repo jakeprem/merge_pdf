47adcbc1a5b9d7d20b10512bb3289dd4e7dc3308
jakeprem <jakeprem@users.noreply.github.com>
2026-01-26 15:13:59 -0500

    update lodpf to 0.39.0 (#4)
    update to 0.39.0

--- mix.exs
+++ mix.exs
@@ -2,7 +2,7 @@ defmodule MergePdf.MixProject do
   use Mix.Project
 
   # Used by CI/CD, so make sure this is updated
-  @version "0.5.1"
+  @version "0.5.2"
   @source_url "https://github.com/jakeprem/merge_pdf"
   @dev? String.ends_with?(@version, "-dev")
   @force_build? System.get_env("MERGE_PDF_BUILD") in ["1", "true"]
